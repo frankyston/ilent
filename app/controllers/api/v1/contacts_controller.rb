c85443d845b651dea2eb3e04ec5d440794513fdb
Frankyston Lins <frankyston@gmail.com>
2020-01-03 11:28:53 -0300

    Adding test CRUD Contacts

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -1,8 +1,46 @@
 class Api::V1::ContactsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_contact, only: [:show, :update, :destroy]
 
   def index
     render json: current_user.contacts, status: :ok
+  end
+
+  def show
+    render json: @contact, status: :ok
+  end
+
+  def create
+    @contact = current_user.contacts.build(contact_params)
+
+    if @contact.save
+      render json: @contact, status: :created
+    else
+      render json: { errors: @contact.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @contact.update(contact_params)
+      render json: @contact, status: :ok
+    else
+      render json: { errors: @contact.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @contact.destroy
+    head 204
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :phone, :email)
+  end
+
+  def set_contact
+    @contact = current_user.contacts.find(params[:id])
   end
 
 end
--- test/controllers/api/v1/contacts_controller_test.rb
+++ test/controllers/api/v1/contacts_controller_test.rb
@@ -1,7 +1,97 @@
 require 'test_helper'
 
 class Api::V1::ContactsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include AuthorizationHelper
+
+  setup do
+    @contact = contacts(:one)
+    @user = @contact.user
+    @token = get_token_for_user(@user)
+  end
+
+  test "should show contacts" do
+    get api_v1_contacts_url, headers: {
+      Authorization: @token
+    }
+
+    assert_response :success
+  end
+
+  test 'should show contact' do
+    get api_v1_contact_url(@contact), headers: {
+      Authorization: @token
+    }, as: :json
+
+    assert_response :success
+  end
+
+  test 'should create contact' do
+    post api_v1_contacts_url, params: {
+      contact: {
+        name: Faker::Name.first_name,
+        phone: Faker::PhoneNumber.cell_phone,
+        email: Faker::Internet.email
+      }
+    }, headers: {
+      Authorization: @token
+    }, as: :json
+
+    assert_response :created
+  end
+
+  test 'should not create contact' do
+    post api_v1_contacts_url, params: {
+      contact: {
+        name: Faker::Name.first_name,
+        phone: Faker::PhoneNumber.cell_phone,
+      }
+    }, headers: {
+      Authorization: @token
+    }, as: :json
+
+    assert_response :unprocessable_entity
+  end
+
+  test 'should update contact' do
+    patch api_v1_contact_url(@contact), params: {
+      contact: {
+        name: Faker::Name.first_name,
+      }
+    }, headers: {
+      Authorization: @token
+    }, as: :json
+
+    assert_response :success
+  end
+
+  test 'should not update contact with email' do
+    patch api_v1_contact_url(@contact), params: {
+      contact: {
+        email: 'bad_email',
+      }
+    }, headers: {
+      Authorization: @token
+    }, as: :json
+
+    assert_response :unprocessable_entity
+  end
+
+  test 'should not update contact' do
+    patch api_v1_contact_url(@contact), params: {
+      contact: {
+        name: Faker::Name.first_name,
+      }
+    }, as: :json
+
+    assert_response :unauthorized
+  end
+
+  test 'should destroy contact' do
+    delete api_v1_contact_url(@contact), 
+    headers: {
+      Authorization: @token
+    }, as: :json
+
+    assert_response :no_content
+  end
 end
