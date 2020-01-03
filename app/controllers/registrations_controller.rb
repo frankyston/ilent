24289c2227f56b5998b9003bf30b8f63ff204981
Frankyston Lins <frankyston@gmail.com>
2020-01-02 23:03:32 -0300

    Overridden methods of sessions and registrations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,25 @@
 class RegistrationsController < Devise::SessionsController
   respond_to :json
+
+  def create
+    build_resource(sign_up_params)
+
+    if resource.save
+      render json: resource, status: :ok
+    else
+      render json: { errors: resource.errors }, status: :unprocessable_entity
+    end
+  end
+
+  protected
+
+  def build_resource(hash = {})
+    self.resource = resource_class.new_with_session(hash, session)
+  end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -40,7 +40,7 @@ Devise.setup do |config|
   # session. If you need permissions, you should implement that in a before filter.
   # You can also supply a hash where the value is a boolean determining whether
   # or not authentication should be aborted when the value is not present.
-  # config.authentication_keys = [:email]
+  config.authentication_keys = [:email]
 
   # Configure parameters from the request object used for authentication. Each entry
   # given should be a request method and it will automatically be passed to the
@@ -307,6 +307,6 @@ Devise.setup do |config|
       jwt.revocation_requests = [
         ['DELETE', %r{^/logout$}]
       ]
-    jwt.expiration_time = 5.minutes.to_i
+    jwt.expiration_time = 1.day.to_i
   end
 end
